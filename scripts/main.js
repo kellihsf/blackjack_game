a79e29aca12bc6517e44e2ec76219b0bb7fc2251
Kelli <kellistone217@gmail.com>
2021-10-28 17:31:57 -0600

    Added event listener

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   // Execute after page load
+
+  // Add a click event listener to the button
+  const dealBtn = document.querySelector("#deal-button");
+  dealBtn.addEventListener("click", function () {
+    console.log("You've pressed the deal button");
+
+    //Create a new card image element
+    const newCard = document.createElement('img');
+    console.log(newCard)
+  });
 })
+
