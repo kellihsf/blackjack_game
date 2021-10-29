4a8d0e4fcfdf38c31aee42172b41415637cfc680
Kelli <kellistone217@gmail.com>
2021-10-29 13:05:46 -0600

    Added event listeners for button clicks

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,48 @@ window.addEventListener('DOMContentLoaded', function () {
   dealBtn.addEventListener("click", function () {
     console.log("You've pressed the deal button");
 
-    //Create a new card image element
+    //Create a new card image element - Dealer
     const newCard = document.createElement('img');
-    console.log(newCard)
+
+    // Append the new card image element to the dealer hand div
+    const dealerHand = document.querySelector("#dealer-hand");
+    dealerHand.appendChild(newCard);
+
+    // Assign new const of newCard variable the source (2 of clubs)
+    newCard.src = 'images/2_of_clubs.png';
+
+
+    //Create a new card image element - Player
+    const newCardPlayer = document.createElement('img');
+
+    // Append the new card image element to the player hand div
+    // Append the player card to the player hand div
+    const playerHand = document.querySelector("#player-hand");
+    playerHand.appendChild(newCardPlayer);
+
+    // Assign new const of newCard variable the source (2 of clubs)
+    newCardPlayer.src = 'images/9_of_hearts.png';
   });
+
+
+  const hitBtn = document.querySelector("#hit-button");
+  hitBtn.addEventListener("click", function () {
+    console.log("You've pressed the hit button");
+
+    //Create a new card image element - Dealer
+    const newCard = document.createElement('img');
+
+    // Append the new card image element to the dealer hand div
+    const dealerHand = document.querySelector("#dealer-hand");
+    dealerHand.appendChild(newCard);
+
+    // Assign new const of newCard variable the source (2 of clubs)
+    newCard.src = 'images/3_of_diamonds.png';
+  })
 })
 
+// deck is array of objects
+// create a function buildDeck that returns a new array with 52 card objects
+function buildDeck () {
+  cardArray = [];
+}
